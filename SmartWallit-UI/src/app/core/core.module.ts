f9aed4d754d24e2ff8a03bbbe16afd67d636cc4b
alan skaev <alan.skaev22@gmail.com>
2021-11-10 20:26:30 -0500

    Added footer and styled router outlet width

--- SmartWallit-UI/src/app/core/core.module.ts
+++ SmartWallit-UI/src/app/core/core.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SharedModule } from '../shared/shared.module';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     SharedModule
   ],
-  exports: [NavbarComponent]
+  exports: [
+    NavbarComponent,
+    FooterComponent]
 })
 export class CoreModule { }
